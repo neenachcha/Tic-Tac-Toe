d49f939526f394282659a59823929fb75f8b90f2
Nachcha Aroonrerk (Neen) <neen.nachcha@gmail.com>
2022-01-06 12:19:23 +0700

    Complete Tic-Tac-Toe Bare Minimum Requirements

--- app.js
+++ app.js
@@ -4,121 +4,150 @@
 3. No CSS
 */
 
-var board;
-var _round = 0;
-
 //==================== MODEL ====================== // manage rules
-// detect win or tie
+var board = document.getElementById('board');
+var _round = 0;
+var resetButtonArr = document.getElementsByClassName('button');
+var element = document.getElementById('winner');
+
 var detectWinOrTie = () => {
-
-  // WIN rules if:
-  // 1. three same mark on the same row
-  // 2. three same mark on the same column
-  // 3. three same mark diagonally top left to bottom right
-  // 4. three same mark diagonally top right to bottom left
-  var winner = detectThreeSameMarkOnSameRow() || detectThreeSameMarkOnSameColumn()
-  || detectThreeSameMarkOnLeftToRightDiagonal
-  || detectThreeSameMarkOnRightToLeftDiagonal();
-
+  var winner = detectEasy() ;
   if (winner) {
-    // call show who wins with input of that mark
     showWhoWins(winner);
+    freezeGame();
     return;
   }
-
-  // if the board has no more space left
   if (_round === 9) {
-    // call show wins with input "tie"
     showWhoWins('tie');
+    freezeGame();
     return;
   }
 };
 
-var detectThreeSameMarkOnSameRow = () => {
-  // if current td has no content
-    // move to the next row
-  // else
-    // if no mark yet, save current content as mark
-    // if mark exist, but mark is not the same as current td content
-      // move to the next row
-    // move to the td in the same row
-
-  return mark; // return mark which represent 'x' 'o' or null
-};
-var detectThreeSameMarkOnSameColumn = () => {
-
-  return mark;
-};
-var detectThreeSameMarkOnLeftToRightDiagonal = () => {
-
-  return mark;
-};
-var detectThreeSameMarkOnRightToLeftDiagonal = () => {
-
-  return mark;
-};
-
-// Rules for next player
 var whoIsNextPlayer = () => {
   var isEven = (num) => {
     return num%2 === 0
   }
-  // First player is always x (odd round)
   if (!isEven(_round)) {
     return 'x'
-  // second play is always o (even round)
   } else {
     return 'o'
   }
 }
 
-//==================== VIEW ====================== // x o and the board
-// show mark (input = clicked element)
+//==================== HELPER ======================
+var getBoardArray = () => {
+  var boardArr = [];
+  board = document.getElementById('board');
+  var trs = board.getElementsByTagName('tr');
+
+  for (var tr of trs) {
+    var tds = tr.getElementsByTagName('td');
+    var rowArr = [];
+    for (var td of tds) {
+      rowArr.push(td.textContent);
+    }
+    boardArr.push(rowArr);
+  }
+  return boardArr;
+};
+
+var detectEasy = () => {
+  // WIN rules if:
+  // 1. three same mark on the same row
+  // 2. three same mark on the same column
+  // 3. three same mark diagonally top left to bottom right
+  // 4. three same mark diagonally top right to bottom left
+  var board = getBoardArray();
+  var mark = '';
+  var detectRow = (r) => {
+    if (r[0] === r[1] && r[1] === r[2]) {
+      mark = r[0];
+    }
+  }
+  var detectCol = (c) => {
+    if (board[0][c] === board[1][c] && board[1][c] === board[2][c]) {
+      mark = board[0][c];
+    }
+  }
+  var detectDiagonalLeft = () => {
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      mark = board[0][0];
+    }
+  }
+  var detectDiagonalRight = () => {
+    if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      mark = board[0][2];
+    }
+  }
+  board.forEach(row => {
+    detectRow(row);
+  });
+  for (var i = 0; i < 3; i++) {
+    detectCol(i);
+  }
+  detectDiagonalLeft();
+  detectDiagonalRight();
+  return mark;
+};
+
+//==================== VIEW ======================
+
 var showMark = (DOMSpace) => {
-  // if current DOM have no mark
   if (!DOMSpace.textContent) {
-    // check next player rules > return x or o
     var nextPlayer = whoIsNextPlayer();
     DOMSpace.textContent = nextPlayer;
-
-    // count number of round at the end of every turn
     _round++;
     document.getElementById('round').textContent = `Round: ${_round}`
   }
 };
 
-// show who wins (input = x or o or tie)
 var showWhoWins = (winner) => {
-  var element = document.getElementById('winner');
-  // if (x or o)
   if (winner !== 'tie') {
-    // add div element says "X or O wins"
     element.textContent = `${winner} is the Winner!!!`;
-  // if (tie)
   } else {
-    // add div element says "tie"
     element.textContent = `Tie!!`;
   }
 };
 
+var freezeGame = () => {
+  board.removeEventListener('click', handleClick);
+  resetButtonArr[0].style.visibility = 'visible';
+}
+
+var newGame = () => {
+  resetButtonArr[0].style.visibility = 'hidden';
+  board.addEventListener('click', handleClick);
+  element.textContent = 'Have fun!'
+  _round = 0;
+
+  var trs = board.getElementsByTagName('tr');
+  for (var tr of trs) {
+    var tds = tr.getElementsByTagName('td');
+    for (var td of tds) {
+      td.textContent = '';
+    }
+  }
+}
 
 
 
 
 //==================== CONTROLLER ====================== // handle events
-
 // handle button to reset game
+resetButtonArr[0].onclick = function (e) {
+  newGame();
+}
 
-
-// Get element that user click
-board = document.getElementById('board');
-// handle click (always start with player x)
-board.onclick = function (e) {
-  // call show mark (input = clicked element)
+var handleClick = function (e) {
   showMark(e.target);
+  detectWinOrTie();
 };
+board.addEventListener('click', handleClick);
+
 
 
 
 // ==================== APP initialize ======================
 detectWinOrTie();
+resetButtonArr[0].style.visibility = 'hidden';
